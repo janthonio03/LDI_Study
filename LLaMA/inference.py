f47beac4136ad08844a5869fa4fb2cf231884658
janthonio03 <janthonio03@khu.ac.kr>
2026-08-05 01:34:25 +0900

    LLaMA 코드필사

--- LLaMA/inference.py
+++ LLaMA/inference.py
@@ -48,7 +48,6 @@ class LLaMA:
         model = Transformer(model_args).to(device)
 
         if load_model:
-
             del checkpoint['rope.freqs']
             model.load_state_dict(checkpoint, strict=True)
             print(f"Loaded state dict in {time.time() - prev_time:.2f}s")
@@ -58,21 +57,16 @@ class LLaMA:
     def text_completion(self, prompts: list[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len: Optional[int] = None):
         if max_gen_len is None:
             max_gen_len = self.args.max_seq_len - 1
-
         prompt_tokens = [self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=False) for prompt in prompts]
-
         batch_size = len(prompt_tokens)
         assert batch_size <= self.args.max_batch_size, f"batch size must be less than or equal to {self.args.max_batch_size}"
         max_prompt_len = max(len(prompt) for prompt in prompt_tokens)
-
         assert max_prompt_len <= self.args.max_seq_len, f"prompt length must be less than or equal to {self.args.max_seq_len}"
         total_len = min(self.args.max_seq_len, max_gen_len + max_prompt_len)
-
 
         pad_id = self.tokenizer.pad_id()
         tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, device=device)
         for k, t in enumerate(prompt_tokens):
-
             tokens[k, : len(t)] = torch.tensor(t, dtype=torch.long, device=device)
 
         eos_reached = torch.tensor([False] * batch_size, device=device)
@@ -82,18 +76,14 @@ class LLaMA:
             with torch.no_grad():
                 logits = self.model.forward(tokens[:, cur_pos-1:cur_pos], cur_pos)
             if temperature > 0:
-
                 probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
                 next_token = self._sample_top_p(probs, top_p)
             else:
-
                 next_token = torch.argmax(logits[:, -1], dim=-1)
 
             next_token = next_token.reshape(-1)
-
             next_token = torch.where(prompt_tokens_mask[:, cur_pos], tokens[:, cur_pos], next_token)
             tokens[:, cur_pos] = next_token
-
             eos_reached |= (~prompt_tokens_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id)
             if all(eos_reached):
                 break
@@ -101,7 +91,6 @@ class LLaMA:
         out_tokens = []
         out_text = []
         for prompt_index, current_prompt_tokens in enumerate(tokens.tolist()):
-
             if self.tokenizer.eos_id in current_prompt_tokens:
                 eos_idx = current_prompt_tokens.index(self.tokenizer.eos_id)
                 current_prompt_tokens = current_prompt_tokens[:eos_idx]
@@ -110,24 +99,14 @@ class LLaMA:
         return (out_tokens, out_text)
 
     def _sample_top_p(self, probs, p):
-
         probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
-
         probs_sum = torch.cumsum(probs_sort, dim=-1)
-
-
         mask = probs_sum - probs_sort > p
-
         probs_sort[mask] = 0.0
-
         probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
-
         next_token = torch.multinomial(probs_sort, num_samples=1)
-
         next_token = torch.gather(probs_idx, -1, next_token)
         return next_token
-
-
 
 if __name__ == '__main__':
     torch.manual_seed(0)
@@ -138,14 +117,12 @@ if __name__ == '__main__':
     prompts = [
         "Simply put, the theory of relativity states that ",
         "If Google was an Italian company founded in Milan, it would",
-
         """Translate English to French:
 
         sea otter => loutre de mer
         peppermint => menthe poivrée
         plush girafe => girafe peluche
         cheese =>""",
-
         """Tell me if the following person is actually Doraemon disguised as human:
         Name: Umar Jamil
         Decision:
